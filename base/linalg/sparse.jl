480951a8c489600e99ff59e8343e3df188830f75
Andreas Noack Jensen <andreasnoackjensen@gmail.com>
2013-10-12 09:30:34 +0200

    Add allocation free matrix multiplaction and solving

--- base/linalg/sparse.jl
+++ base/linalg/sparse.jl
@@ -8,59 +8,51 @@ function (*){TvA,TiA,TvB,TiB}(A::SparseMatrixCSC{TvA,TiA}, B::SparseMatrixCSC{Tv
     return A * B
 end
 
-(*){TvA,TiA}(A::SparseMatrixCSC{TvA,TiA}, X::BitArray{1}) = invoke(*, (SparseMatrixCSC, AbstractVector), A, X)
 # In matrix-vector multiplication, the correct orientation of the vector is assumed.
 function A_mul_B!(α::Number, A::SparseMatrixCSC, x::AbstractVector, β::Number, y::AbstractVector)
     if A.n != length(x); throw(DimensionMismatch()); end
-    for i = 1:A.n; y[i] *= β; end
+    for i = 1:length(y); y[i] *= β; end
     for col = 1 : A.n, k = A.colptr[col] : (A.colptr[col+1]-1)
         y[A.rowval[k]] += α*A.nzval[k]*x[col]
     end
     return y
 end
+(*){TvA,TiA}(A::SparseMatrixCSC{TvA,TiA}, X::BitArray{1}) = invoke(*, (SparseMatrixCSC, AbstractVector), A, X)
 (*){TA,S,Tx}(A::SparseMatrixCSC{TA,S}, x::AbstractVector{Tx}) = A_mul_B!(1, A, x, 0, zeros(promote_type(TA,Tx), length(x)))
 
-(*)(X::BitArray{1}, A::SparseMatrixCSC) = invoke(*, (AbstractVector, SparseMatrixCSC), X, A)
-# In vector-matrix multiplication, the correct orientation of the vector is assumed.
-# XXX: this is wrong (i.e. not what Arrays would do)!!
-function (*){T1,T2}(X::AbstractVector{T1}, A::SparseMatrixCSC{T2})
-    if A.m != length(X); error("mismatched dimensions"); end
-    Y = zeros(promote_type(T1,T2), A.n)
-    for col = 1 : A.n, k = A.colptr[col] : (A.colptr[col+1]-1)
-        Y[col] += X[A.rowval[k]] * A.nzval[k]
-    end
-    return Y
-end
-
-(*){TvA,TiA}(A::SparseMatrixCSC{TvA,TiA}, X::BitArray{2}) = invoke(*, (SparseMatrixCSC, AbstractMatrix), A, X)
-function (*){TvA,TiA,TX}(A::SparseMatrixCSC{TvA,TiA}, X::AbstractMatrix{TX})
-    mX, nX = size(X)
-    if A.n != mX; error("mismatched dimensions"); end
-    Y = zeros(promote_type(TvA,TX), A.m, nX)
-    for multivec_col = 1:nX
+# Matrix-matrix
+A_mul_B!(α::Number, A::SparseMatrixCSC, B::SparseMatrixCSC, β::Number, C::AbstractMatrix) = error("Not handled yet!")
+function A_mul_B!(α::Number, A::SparseMatrixCSC, B::AbstractMatrix, β::Number, C::AbstractMatrix)
+    mB, nB = size(B)
+    if A.n != mB; throw(DimensionMismatch("")); end
+    for i = 1:length(C); C[i] *= β; end
+    for multivec_col = 1:nB
         for col = 1 : A.n
             for k = A.colptr[col] : (A.colptr[col+1]-1)
-                Y[A.rowval[k], multivec_col] += A.nzval[k] * X[col, multivec_col]
+                C[A.rowval[k], multivec_col] += α*A.nzval[k]*B[col, multivec_col]
             end
         end
     end
-    return Y
+    return C
 end
+(*){TvA,TiA}(A::SparseMatrixCSC{TvA,TiA}, X::BitArray{2}) = invoke(*, (SparseMatrixCSC, AbstractMatrix), A, X)
+(*){TA,S,TB}(A::SparseMatrixCSC{TA,S}, B::AbstractMatrix{TB}) = A_mul_B!(1, A, B, 0, zeros(promote_type(TA,TB), size(A,1), size(B,2)))
 
 (*){TvA,TiA}(X::BitArray{2}, A::SparseMatrixCSC{TvA,TiA}) = invoke(*, (AbstractMatrix, SparseMatrixCSC), X, A)
-function (*){TX,TvA,TiA}(X::AbstractMatrix{TX}, A::SparseMatrixCSC{TvA,TiA})
-    mX, nX = size(X)
-    if nX != A.m; error("mismatched dimensions"); end
-    Y = zeros(promote_type(TX,TvA), mX, A.n)
-    for multivec_row = 1:mX
+function A_mul_B!(α::Number, B::AbstractMatrix, A::SparseMatrixCSC, β::Number, C::AbstractMatrix)
+    mB, nB = size(B)
+    if nB != A.m; throw(DimensionMismatch("")); end
+    for i = 1:length(C); C[i] *= β; end
+    for multivec_row = 1:mB
         for col = 1 : A.n
             for k = A.colptr[col] : (A.colptr[col+1]-1)
-                Y[multivec_row, col] += X[multivec_row, A.rowval[k]] * A.nzval[k]
+                C[multivec_row, col] += α*B[multivec_row, A.rowval[k]] * A.nzval[k]
             end
         end
     end
-    return Y
+    return C
 end
+(*){TA,S,TB}(B::AbstractMatrix{TB}, A::SparseMatrixCSC{TA,S}) = A_mul_B!(1, B, A, 0, zeros(promote_type(TA,TB), size(B,1), size(A,2)))
 
 # Sparse matrix multiplication as described in [Gustavson, 1978]:
 # http://www.cse.iitb.ac.in/graphics/~anand/website/include/papers/matrix/fast_matrix_mul.pdf
